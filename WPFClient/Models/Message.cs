6d59c076d3b840a15299d8378bf44564be38ae7e
Dylan Piette <44063839+Kamigoro@users.noreply.github.com>
2020-10-04 00:42:25 +0200

    Creating message class

---
+++ WPFClient/Models/Message.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChatClient.Models
+{
+    public class Message
+    {
+        public string Username { get; set; }
+        public string Text { get; set; }
+
+        public Message(string username, string text)
+        {
+            Username = username;
+            Text = text;
+        }
+
+    }
+}
